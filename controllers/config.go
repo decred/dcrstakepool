2098896a20483765f8da3e61cc513cd51963178a
(⌐■_■) <jolan@users.noreply.github.com>
2016-04-01 21:40:20 -0500

    provide a better testnet server pool example

--- controllers/config.go
+++ controllers/config.go
@@ -13,17 +13,17 @@ var serverPoolCfg = []byte(`
 [
     {
         "id": 1,
-        "host": "127.0.0.1:18554",
+        "host": "127.0.0.1:19110",
         "user": "USER",
         "pass": "PASSWORD",
-        "cert": "\/home\/mydesktop\/.dcrwallet\/rpc.cert"
+        "cert": "\/home\/mydesktop\/.dcrstakepool\/wallet1.cert"
     },
     {
         "id": 2,
-        "host": "127.0.0.2:18558",
+        "host": "127.0.0.2:19110",
         "user": "USER",
         "pass": "PASSWORD",
-        "cert": "\/home\/mydesktop\/.dcrwallet2\/rpc.cert"
+        "cert": "\/home\/mydesktop\/.dcrstakepool\/wallet2.cert"
     }
 ]
 `)
