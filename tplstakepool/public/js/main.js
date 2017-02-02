3724d0181f65d4261b79c656be332430604264e6
karamble <hello@d3c.red>
2017-02-02 21:41:50 +0100

    dcrstakepool v1.0.0 Template

---
+++ tplstakepool/public/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+$("#inputPassword3").data('holder',$("#inputPassword3").attr('placeholder'));
+$("#inputPassword3").focusin(function(){
+    $(this).attr('placeholder','');
+});
+$("#inputEmail3").data('holder',$("#inputEmail3").attr('placeholder'));
+$("#inputEmail3").focusin(function(){
+    $(this).attr('placeholder','');
+});
+$("#inputEmail3,#inputPassword3").focusout(function(){
+    $(this).attr('placeholder',$(this).data('holder'));
+});
+
+})
