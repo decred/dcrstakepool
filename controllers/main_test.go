9e061ad7c045eb57c71f495a5cf69ac623533039
chappjc <jonathan.chappelow@gmail.com>
2017-06-14 23:05:00 -0700

    add ticket sort benchmarks

--- controllers/main_test.go
+++ controllers/main_test.go
@@ -74,3 +74,65 @@ func TestSortByTicketHeight(t *testing.T) {
 		t.Error("Sort with ByTicketHeight failed")
 	}
 }
+
+func BenchmarkSortByTicketHeight100(b *testing.B)   { benchmarkSortByTicketHeight(100, b) }
+func BenchmarkSortByTicketHeight500(b *testing.B)   { benchmarkSortByTicketHeight(500, b) }
+func BenchmarkSortByTicketHeight1000(b *testing.B)  { benchmarkSortByTicketHeight(1000, b) }
+func BenchmarkSortByTicketHeight2500(b *testing.B)  { benchmarkSortByTicketHeight(2500, b) }
+func BenchmarkSortByTicketHeight5000(b *testing.B)  { benchmarkSortByTicketHeight(5000, b) }
+func BenchmarkSortByTicketHeight10000(b *testing.B) { benchmarkSortByTicketHeight(10000, b) }
+func BenchmarkSortByTicketHeight20000(b *testing.B) { benchmarkSortByTicketHeight(20000, b) }
+
+func benchmarkSortByTicketHeight(ticketCount int, b *testing.B) {
+	// Create a large list of tickets to sort, voted over many blocks
+	maxTxHeight := int64(53000)
+
+	ticketInfoLive := make([]TicketInfoLive, 0, ticketCount)
+	for i := 0; i < ticketCount; i++ {
+		ticketInfoLive = append(ticketInfoLive, TicketInfoLive{
+			TicketHeight: uint32(mrand.Int63n(maxTxHeight)),
+			Ticket:       randHashString(), // could be nothing unless we sort with it
+		})
+	}
+
+	for i := 0; i < b.N; i++ {
+		// Make a copy to sort
+		ticketInfoLive2 := make([]TicketInfoLive, len(ticketInfoLive))
+		copy(ticketInfoLive2, ticketInfoLive)
+
+		// Sort with ByTicketHeight, the test subject
+		sort.Sort(ByTicketHeight(ticketInfoLive2))
+	}
+}
+
+func BenchmarkSortBySpentByHeight100(b *testing.B)   { benchmarkSortBySpentByHeight(100, b) }
+func BenchmarkSortBySpentByHeight500(b *testing.B)   { benchmarkSortBySpentByHeight(500, b) }
+func BenchmarkSortBySpentByHeight1000(b *testing.B)  { benchmarkSortBySpentByHeight(1000, b) }
+func BenchmarkSortBySpentByHeight2500(b *testing.B)  { benchmarkSortBySpentByHeight(2500, b) }
+func BenchmarkSortBySpentByHeight5000(b *testing.B)  { benchmarkSortBySpentByHeight(5000, b) }
+func BenchmarkSortBySpentByHeight10000(b *testing.B) { benchmarkSortBySpentByHeight(10000, b) }
+func BenchmarkSortBySpentByHeight20000(b *testing.B) { benchmarkSortBySpentByHeight(20000, b) }
+
+func benchmarkSortBySpentByHeight(ticketCount int, b *testing.B) {
+	// Create a large list of tickets to sort, voted over many blocks
+	maxTxHeight := int64(53000)
+
+	ticketInfoVoted := make([]TicketInfoHistoric, 0, ticketCount)
+	for i := 0; i < ticketCount; i++ {
+		ticketInfoVoted = append(ticketInfoVoted, TicketInfoHistoric{
+			Ticket:        randHashString(), // could be nothing unless we sort with it
+			SpentBy:       randHashString(),
+			SpentByHeight: uint32(mrand.Int63n(maxTxHeight)),
+			TicketHeight:  uint32(mrand.Int63n(maxTxHeight)),
+		})
+	}
+
+	for i := 0; i < b.N; i++ {
+		// Make a copy to sort
+		ticketInfoVoted2 := make([]TicketInfoHistoric, len(ticketInfoVoted))
+		copy(ticketInfoVoted2, ticketInfoVoted)
+
+		// Sort with BySpentByHeight, the test subject
+		sort.Sort(BySpentByHeight(ticketInfoVoted2))
+	}
+}
