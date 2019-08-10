cd834c0b1355c315548ede87604384a87719d5c8
peaceville <uferepeace@gmail.com>
2019-08-10 14:45:05 +0100

    fix index-out-of-range crashes when no values are provided for adminuserids, wallethosts, walletusers, walletpasswords and walletcerts in dcrstakepool.conf

--- config.go
+++ config.go
@@ -559,11 +559,21 @@ func loadConfig() (*config, []string, error) {
 	if len(cfg.AdminIPs) > 0 {
 		cfg.AdminIPs = strings.Split(cfg.AdminIPs[0], ",")
 	}
-	cfg.AdminUserIDs = strings.Split(cfg.AdminUserIDs[0], ",")
-	cfg.WalletHosts = strings.Split(cfg.WalletHosts[0], ",")
-	cfg.WalletUsers = strings.Split(cfg.WalletUsers[0], ",")
-	cfg.WalletPasswords = strings.Split(cfg.WalletPasswords[0], ",")
-	cfg.WalletCerts = strings.Split(cfg.WalletCerts[0], ",")
+	if len(cfg.AdminUserIDs) > 0 {
+		cfg.AdminUserIDs = strings.Split(cfg.AdminUserIDs[0], ",")
+	}
+	if len(cfg.WalletHosts) > 0 {
+		cfg.WalletHosts = strings.Split(cfg.WalletHosts[0], ",")
+	}
+	if len(cfg.WalletUsers) > 0 {
+		cfg.WalletUsers = strings.Split(cfg.WalletUsers[0], ",")
+	}
+	if len(cfg.WalletPasswords) > 0 {
+		cfg.WalletPasswords = strings.Split(cfg.WalletPasswords[0], ",")
+	}
+	if len(cfg.WalletCerts) > 0 {
+		cfg.WalletCerts = strings.Split(cfg.WalletCerts[0], ",")
+	}
 
 	// Add default wallet port for the active network if there's no port specified
 	cfg.WalletHosts = normalizeAddresses(cfg.WalletHosts, activeNetParams.WalletRPCServerPort)
